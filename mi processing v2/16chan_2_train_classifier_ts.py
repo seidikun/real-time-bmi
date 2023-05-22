a7625a17363697bef1fb45a1061455d25ff54f49
Seidi Yonamine <seidi.yamauti@gmail.com>
2023-05-22 11:19:42 -0300

    updates

--- mi processing v2/16chan_2_train_classifier_ts.py
+++ mi processing v2/16chan_2_train_classifier_ts.py
@@ -1,24 +1,19 @@
-# -*- coding: utf-8 -*-
-
 import pickle
 import random
 import numpy  as np
 import pandas as pd
-
+import configparser 
 from scipy                         import signal
 from sklearn.metrics               import cohen_kappa_score
-from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.linear_model          import LogisticRegression
 from pyriemann.estimation          import Covariances
 from pyriemann.tangentspace        import tangent_space
 from pyriemann.utils.mean          import mean_covariance
 
-import configparser 
-
-configParser = configparser.RawConfigParser()   
-configFilePath = r'.\config.txt'
+# Read config file
+configParser    = configparser.RawConfigParser()   
+configFilePath  = r'.\config.txt'
 configParser.read(configFilePath)
-
 Experiment      = configParser['PARAMETERS']['Experiment']
 Participant     = configParser['PARAMETERS']['Participant']
 Session_nb      = configParser['PARAMETERS']['Session_nb']
@@ -29,30 +24,29 @@ filename        = sess_filename + '.csv'
 pkl_filename    = sess_filename + '_lda.pkl'
 c_mean_filename = sess_filename + '_best_c_mean.pkl'
 
-#################### TROQUE O NOME DO ARQUIVO AQUI ###########################
-
+# ------------------------------ PRE-PROCESSING ------------------------------#
 # Parameters
-overlap_step = 0.1  # tamanho do passo para sobreposição de janelas
-max_trial_t  = 3.75 # tamanho máximo de uma janela de tentativa
-b, a         = signal.butter(4, (8,30), 'bandpass', fs=512)
+overlap_step    = 0.0625  # Temporal epoch overlap
+max_trial_t     = 3.75 # Trial nmax duration
+b, a            = signal.butter(4, (8,30), 'bandpass', fs=512)
 
 # Read .csv file
-df_me        = pd.read_csv(filename)
+df_me           = pd.read_csv(filename)
 
 # Get indexes of left MI and right MI
-event_id     = pd.to_numeric(df_me['Event Id'], errors='coerce').fillna(0).astype(np.int64)
-left_inds    = event_id.index[event_id == 769].tolist()
-right_inds   = event_id.index[event_id == 770].tolist()
+event_id        = pd.to_numeric(df_me['Event Id'], errors='coerce').fillna(0).astype(np.int64)
+left_inds       = event_id.index[event_id == 769].tolist()
+right_inds      = event_id.index[event_id == 770].tolist()
   
 # Sample frequency
-Fs           = 1/np.mean(np.diff(df_me['Time:512Hz']))
+Fs              = 1/np.mean(np.diff(df_me['Time:512Hz']))
 
 # Get EEG data only and band-pass filter
-data_eeg     = df_me.iloc[:, 2:18].values
-data_eeg     = signal.filtfilt(b, a, data_eeg.T).T
+data_eeg        = df_me.iloc[:, 2:18].values
+data_eeg        = signal.filtfilt(b, a, data_eeg.T).T
     
 # Time vector of epoch
-trange       = np.arange(int(Fs*0.5),int(Fs*2.5), 4) # O 4 é pra realizar um downsample por 4
+trange          = np.arange(int(Fs*0.5),int(Fs*2.5), 4) # O 4 é pra realizar um downsample por 4
   
 # Windowing
 while trange[-1] < int(max_trial_t*Fs):        
@@ -67,7 +61,7 @@ while trange[-1] < int(max_trial_t*Fs):
   ts            = [i + trange for i in right_inds]
   epochs_right  = data_eeg[ts,:]
   epochs_mne    = np.vstack([epochs_left, epochs_right])
-  labels        = np.append(labels, [1 for i in right_inds])
+  labels        = np.append(labels,    [1 for i in right_inds])
   trial_num     = np.append(trial_num, [i for i in range(len(right_inds))])
 
   # Create epochs stack, labels array and trial id array
@@ -83,98 +77,90 @@ while trange[-1] < int(max_trial_t*Fs):
   # Slide window
   trange += int(overlap_step*Fs)
 
-# Randomization
+# Trials randomization
 p             = np.random.permutation(len(all_labels))
 data          = np.swapaxes(data[p,:,:,],1,2)
 all_labels    = all_labels[p]
 all_trial_num = all_trial_num[p]
 
+# Unique trial id
+unique_labels = np.unique(all_trial_num)
 
-# Inicializar variáveis
+# ------------------------------ MODEL VALIDATION ----------------------------#
+# Init variables
 best_accuracy = 0
 best_kappa    = 0
 best_c_mean   = None
 n_folds       = 20
 
-# Criar lista com os 20 rótulos únicos de trial
-unique_labels = np.unique(all_trial_num)
-
-# Inicializar array para armazenar resultados da validação cruzada
+# Init metrics arrays
 accuracies    = np.zeros(n_folds)
 kappas        = np.zeros(n_folds)
 
-# Fazer a validação cruzada
+# Cross-validation
 for fold in range(n_folds):
 
-    # Escolher aleatoriamente 2 rótulos para o conjunto de validação
-    val_labels = random.sample(list(unique_labels), 2)
+    # Randomly choose 2 trial ids for validation
+    val_labels       = random.sample(list(unique_labels), 2)
 
-    # Selecionar os índices dos dados de treinamento e validação
-    train_inds           = np.where(np.logical_not(np.isin(all_trial_num, val_labels)))[0]
-    val_inds             = np.where(np.isin(all_trial_num, val_labels))[0]
+    # Trials indexes
+    train_inds       = np.where(np.logical_not(np.isin(all_trial_num, val_labels)))[0]
+    val_inds         = np.where(np.isin(all_trial_num, val_labels))[0]
 
-    # Transformar os dados em matrizes de covariância
-    cov_data_train       = Covariances().transform(data[train_inds])
-    cov_data_val         = Covariances().transform(data[val_inds])
+    # Transform to covariance matrices
+    cov_data_train   = Covariances().transform(data[train_inds])
+    cov_data_val     = Covariances().transform(data[val_inds])
     
-    # Dividir os dados em conjunto de treinamento, validação e teste
-    X_train, y_train     = cov_data_train, all_labels[train_inds]
-    X_val, y_val         = cov_data_val, all_labels[val_inds]
+    # Train-validation split
+    X_train, y_train = cov_data_train, all_labels[train_inds]
+    X_val, y_val     = cov_data_val, all_labels[val_inds]
     
-    # Calcular a média das matrizes de covariância do conjunto de treinamento
-    C_mean               = mean_covariance(X_train)
+    # Mean covariance matrix
+    C_mean           = mean_covariance(X_train)
     
-    # Projetar as matrizes de covariância no plano tangencial
-    tan_space_covs_train = tangent_space(X_train, C_mean)
-    tan_space_covs_val   = tangent_space(X_val, C_mean)
+    # Project matrices to tangential space
+    tan_space_train  = tangent_space(X_train, C_mean)
+    tan_space_val    = tangent_space(X_val, C_mean)
     
-    # Treinar o modelo de LDA no conjunto de treinamento
-    lda = LinearDiscriminantAnalysis()
-    lda = LogisticRegression(random_state=0)
-
-    lda.fit(tan_space_covs_train, y_train)
+    # Train classifier
+    classifier = LogisticRegression(random_state=0)
+    classifier.fit(tan_space_train, y_train)
     
-    # Avaliar a precisão do modelo no conjunto de validação
-    accuracy = lda.score(tan_space_covs_val, y_val)
-    print("\nFold", fold+1, "- Acurácia do modelo no conjunto de validação:", accuracy)
+    # Metrics
+    print('\nFold', fold+1)
+    accuracy         = classifier.score(tan_space_val, y_val)
+    print('Accuracy:', accuracy)
     
-    y_pred_val = lda.predict(tan_space_covs_val)
-    kappa_val  = cohen_kappa_score(y_val, y_pred_val)
-    print("Fold", fold+1, "- Kappa do modelo no conjunto de validação:", kappa_val)
+    y_pred_val       = classifier.predict(tan_space_val)
+    kappa_val        = cohen_kappa_score(y_val, y_pred_val)
+    print('Kappa:   ', kappa_val)
     
-    # Armazenar a precisão do modelo no conjunto de validação
     accuracies[fold] = accuracy
     kappas[fold]     = kappa_val
 
-    # Se a precisão do modelo atual for melhor que a anterior, salvar a matriz de projeção
+    # Select best c mean according to kappa
     if kappa_val > best_kappa:
-        best_kappa = kappa_val
-        best_c_mean   = C_mean
+        best_kappa  = kappa_val
+        best_c_mean = C_mean
 
-# Calcular a precisão média do modelo na validação cruzada
-mean_accuracy = np.mean(accuracies)
-print("\nPrecisão média do modelo na validação cruzada:", mean_accuracy)
+# Calculate mean accuracy
+print("\nMean cross-validation accuracy:", np.mean(accuracies))
+print("Mean cross-validation kappa:   ", np.mean(kappas))
 
 # Saving the objects:
 with open(c_mean_filename, 'wb') as f: 
     pickle.dump(best_c_mean, f)
 
-# Transformar os dados em matrizes de covariância
-cov_data_train       = Covariances().transform(data)
+# ------------------------------ MODEL TRAINING ------------------------------#
+# Transform epochs into covariance matrices
+cov_data_train  = Covariances().transform(data)
 
-# Calcular a média das matrizes de covariância do conjunto de treinamento
-C_mean               = mean_covariance(cov_data_train)
+# Project cov matrices to tangential space trhough best_c_mean
+tan_space_train = tangent_space(cov_data_train, best_c_mean)
 
-# Projetar as matrizes de covariância no plano tangencial
-tan_space_covs_train = tangent_space(cov_data_train, C_mean)
-
-# Treinar o modelo de LDA no conjunto de treinamento
-lda = LinearDiscriminantAnalysis()
-lda.fit(tan_space_covs_train, all_labels)
-
-clf = LogisticRegression().fit(tan_space_covs_train, all_labels)
+# Train classifier
+classifier      = LogisticRegression().fit(tan_space_train, all_labels)
 
 # Save to file in the current working directory
 with open(pkl_filename, 'wb') as file:
-    pickle.dump(clf, file)
-
+    pickle.dump(classifier, file)
